4f95a8787b63ccfe635147bb9e2fe3e0608ed389
Mostafa Fayed <fayedmostafa5@gmail.com>
2024-08-06 17:37:59 +0300

    adding booking & category Schema

--- models/category.js
+++ models/category.js
@@ -1,1 +1,27 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+            id: {
+                        type: String,
+                        required: true,
+                        unique: true 
+            },
+            name: {
+                        type: String,
+                        required: true
+            },
+            description: {
+                        type: String 
+            },
+            icon: {
+                        type: String 
+            },
+            listingCount: {
+                        type: Number,
+                        default: 0 
+            }
+});
+
+const Category = mongoose.model('Category', CategorySchema);
+
+export default Category;
