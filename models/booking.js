26f126b5b38d48757789c7be17fc51f6503f3ef5
Mostafa Fayed <fayedmostafa5@gmail.com>
2024-08-06 16:30:52 +0300

    first commit

---
+++ models/booking.js
@@ -0,0 +1,20 @@
+
+import mongoose from "mongoose";
+
+
+
+let todoSchema = mongoose.Schema({
+            title: {
+                        type: String,
+                        required: true,
+                        minLength: 5,
+                        maxLength: 20
+            },
+            status: {
+                        type: String,
+                        enum: ['todo', 'in_progress', 'done'],
+                        default: "todo",
+            }})
+let TodosModel = mongoose.model("todo", todoSchema)
+export default TodosModel
+
