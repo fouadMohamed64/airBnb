ba26e7695bd94d91ad3f2e8372e41947fb5bee68
fouadMohamed <fm5669653@gmail.com>
2024-08-07 17:28:57 +0300

    ..

--- routes/listings.router.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { getAllListings } from '../controllers/listings.controller.js'
-
-export const listingRoutes = express.Router();
-
-listingRoutes.get('/listings', getAllListings)
