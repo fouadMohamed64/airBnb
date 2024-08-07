a167ead5ed4d3cd1cdd9fb9eb8b8e1006a3b13c8
Mostafa Fayed <fayedmostafa5@gmail.com>
2024-08-07 11:22:31 +0300

    Empty-Commit

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user';
 
-
-
-
 const app = express();
-const URL = 'mongodb+srv://teamfeammearn:IchwrnfkrNZ7Jark@cluster0.0xq8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' 
-mongoose.connect(URL)
-            .then(_ => console.log('connected to db successfully'))
-            .catch(e => console.log(e))
+const URL = 'mongodb+srv://teamfeammearn:IchwrnfkrNZ7Jark@cluster0.0xq8q.mongodb.net/yourDatabaseNameHere?retryWrites=true&w=majority';
 
+// Connect to MongoDB
+mongoose.connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+}).then(() => console.log('Connected to MongoDB'))
+.catch(e => console.error('Connection error:', e));
 
+app.use(express.json());
 
-app.use(express.json())
-
+// Routes
 app.use('/users', userRoutes);
 
-
-let port = 4000;
-app.listen(port, _ => console.log(`listening on ${port}`))
+const port = 4000;
+app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+});
