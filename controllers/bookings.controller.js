37dd7c68f97309091c7fbacd90ea6771b800ed49
EyadAymanM <eiadaiman51@gmail.com>
2024-08-09 04:19:05 +0300

    populating the ref types

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -1,31 +1,46 @@
 import Booking from "../models/booking.model";
 
 export const createBooking = async (req, res) => {
-    const newBooking = await Booking.create(req.body);
-    res.status(201).json({message:"Added Successfully", data: newBooking });
+    const newBooking = await Booking.create(req.body)
+        .populate('listingId', 'title _id')
+        .populate('guestId', 'firstName lastName _id')
+        .populate('hostId', 'firstName lastName _id');
+    res.status(201).json({ message: "Added Successfully", data: newBooking });
 }
 
 export const getBookingsForGuestId = async (req, res) => {
-    const bookings = await Booking.find({guestId: req.params.id});
-    res.status(200).json({message:"sucsses", data: bookings });
+    const bookings = await Booking.find({ guestId: req.params.id })
+        .populate('listingId', 'title _id')
+        .populate('guestId', 'firstName lastName _id')
+        .populate('hostId', 'firstName lastName _id');
+    res.status(200).json({ message: "sucsses", data: bookings });
 }
 
 export const getAllBookings = async (req, res) => {
-    const bookings = await Booking.find();
-    res.status(200).json({message:"sucsses", data: bookings });
+    const bookings = await Booking.find()
+        .populate('listingId', 'title _id')
+        .populate('guestId', 'firstName lastName _id')
+        .populate('hostId', 'firstName lastName _id');
+    res.status(200).json({ message: "sucsses", data: bookings });
 }
 
 export const getBookingById = async (req, res) => {
-    const booking = await Booking.findById(req.params.id);
-    res.status(200).json({message:"sucsses", data: booking });
+    const booking = await Booking.findById(req.params.id)
+        .populate('listingId', 'title _id')
+        .populate('guestId', 'firstName lastName _id')
+        .populate('hostId', 'firstName lastName _id');
+    res.status(200).json({ message: "sucsses", data: booking });
 }
 
 export const updateBooking = async (req, res) => {
-    const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(206).json({message:"edited", data: booking });
+    const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .populate('listingId', 'title _id')
+        .populate('guestId', 'firstName lastName _id')
+        .populate('hostId', 'firstName lastName _id');
+    res.status(206).json({ message: "edited", data: booking });
 }
 
 export const deleteBooking = async (req, res) => {
     await Booking.findByIdAndDelete(req.params.id)
-    res.status(204).json({ message: "deleted"});
+    res.status(204).json({ message: "deleted" });
 }
