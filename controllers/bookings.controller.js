db339c8711ce4f734db9b3ecfa742eeffc87cf4d
Mostafa Fayed <fayedmostafa5@gmail.com>
2024-08-09 05:20:19 +0300

    WIP: 56fcd8d Resolved merge conflicts

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -1,5 +1,31 @@
-import Booking from "../models/booking.model";
+import Booking from "../models/booking.model.js";
 
 export const createBooking = async (req, res) => {
-    
+    const newBooking = await Booking.create(req.body);
+    res.status(201).json({message:"Added Successfully", data: newBooking });
+}
+
+export const getBookingsForGuestId = async (req, res) => {
+    const bookings = await Booking.find({guestId: req.params.id});
+    res.status(200).json({message:"sucsses", data: bookings });
+}
+
+export const getAllBookings = async (req, res) => {
+    const bookings = await Booking.find();
+    res.status(200).json({message:"sucsses", data: bookings });
+}
+
+export const getBookingById = async (req, res) => {
+    const booking = await Booking.findById(req.params.id);
+    res.status(200).json({message:"sucsses", data: booking });
+}
+
+export const updateBooking = async (req, res) => {
+    const booking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(206).json({message:"edited", data: booking });
+}
+
+export const deleteBooking = async (req, res) => {
+    await Booking.findByIdAndDelete(req.params.id)
+    res.status(204).json({ message: "deleted"});
 }
